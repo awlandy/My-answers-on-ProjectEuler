b7438015bd09856bed6723e2fe5e2f65f441baa4
Andy <40180962+awlandy@users.noreply.github.com>
2019-05-03 16:11:04 +0800

    Create ep3.cpp

---
+++ myanswers/cpp/ep3/ep3.cpp
@@ -0,0 +1,18 @@
+//What is the largest prime factor of the number 600851475143 ?
+//#include "pch.h"
+
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include "Header.h"
+
+std::list<long long> Factorset::primeFactors;
+std::list<long long> Factorset::nonprimeFactors;
+std::list<long long> Factorset::factors;
+
+int main() {
+	const long long subject = 600851475143;
+	Factorset setone(600851475143);
+	setone.printPrimelist();
+	return true;
+}
